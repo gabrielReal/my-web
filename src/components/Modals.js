6bf77034b950e7a822312a4135e0c36128dddd6a
D3press3d <anthonyrosman20@gmail.com>
2023-02-26 13:16:25 -0400

    improve filter projects and UX in the buttons with pointer events

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,161 +1,153 @@
 import React from 'react';
-import { useState, useEffect } from "react";
-import { useModal } from "../hooks/useModal";
-import { Carousel } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Modal from "./Modal";
-import projectsDB from "./PortfolioDB";
+import { useState } from 'react';
+import { useModal } from '../hooks/useModal';
+import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from './Modal';
+import projectsDB from './PortfolioDB';
 
+function Modals({
+  client,
+  type,
+  web,
+  link,
+  description,
+  description2,
+  description3,
+  description4,
+  description5,
+  description6,
+  image,
+  slide01,
+  slide02,
+}) {
+  const [isOpenModal, openModal, closeModal] = useModal(false);
 
-  
-  function Modals({ client, type, web, link, description, description2, description3, description4, description5, description6, image, slide01, slide02 }) {
-    
-    const [isOpenModal, openModal, closeModal] = useModal(false);
+  return (
+    <>
+      <article className="PortfolioArticle">
+        <div className="cuadrado"></div>
+        <figure>
+          <figcaption className="PortfoliovViewMore" onClick={openModal}>
+            +
+          </figcaption>
+          <img src={image} alt={type} title={type} />
+        </figure>
+      </article>
 
-    return (
-    <>  
+      <Modal isOpen={isOpenModal} closeModal={closeModal}>
+        <article className="fotos">
+          <Carousel fade>
+            <Carousel.Item>
+              <img className="d-block w-100" src={slide01} alt={description} />
+            </Carousel.Item>
 
-    
-             
-        <article className="PortfolioArticle">
-            <div className="cuadrado"></div>
-            <figure>
-            <figcaption className="PortfoliovViewMore" onClick={openModal}>+</figcaption>
-                <img src={image} alt={type} title={type} />
-            </figure>
+            <Carousel.Item>
+              <img className="d-block w-100" src={slide02} alt={description} />
+            </Carousel.Item>
+          </Carousel>
         </article>
-
-        <Modal isOpen={isOpenModal} closeModal={closeModal}>
-            <article className="fotos">
-            <Carousel fade>
-
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src={slide01}
-                  alt={description}
-                />
-              </Carousel.Item>
-
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src={slide02}
-                  alt={description}
-                />
-              </Carousel.Item>
-              
-            </Carousel>
-            </article>
-            <article className="fotos-textos">
-                <h2>{client}</h2>
-                <h1>{type}</h1>	
-                    <nav>
-                        <ul>
-                            <li><a href={link} target="_blank" rel="noopener noreferrer">{web}</a></li>
-                            <li>{description}</li>
-                            <li>{description2}</li>
-                            <li>{description3}</li>
-                            <li>{description4}</li>
-                            <li>{description5}</li>
-                            <li>{description6}</li>
-                        </ul>
-                    </nav>
-                    
-            </article>
-   
+        <article className="fotos-textos">
+          <h2>{client}</h2>
+          <h1>{type}</h1>
+          <nav>
+            <ul>
+              <li>
+                <a href={link} target="_blank" rel="noopener noreferrer">
+                  {web}
+                </a>
+              </li>
+              <li>{description}</li>
+              <li>{description2}</li>
+              <li>{description3}</li>
+              <li>{description4}</li>
+              <li>{description5}</li>
+              <li>{description6}</li>
+            </ul>
+          </nav>
+        </article>
       </Modal>
-      
     </>
-  )};
+  );
+}
+// ESTO QUE ESTAS EXPORTANDO AQUI ABAJO LO ESTAS USANDO COMO 'MODALS' EN Portfolio.js, tienes que cambiar los nombres para que tengas consistencia porque sino te va a costar un mundo debuguear
 
-  export default function AjaxHooks() {
-    const [filter, setFilter] = useState("all");
-    const [show, setShow] = useState(false);
-    const [projects, setProjects] = useState([]);
+export default function AjaxHooks() {
+  const [filter, setFilter] = useState('all');
+  const [projectFakePagination,setProjectFakePagination] =useState(10)
 
-    useEffect(() => {
+  const handlePagination = ()=>{
+    if(projectFakePagination >= filteredProjects.length){
+      setProjectFakePagination(10)
+      return
+    }   
+    setProjectFakePagination((prev)=>prev+3)
+  }
 
-      const getProjects = () => {
- 
-        projectsDB.forEach((el) => {
-  
-          let project = {
-            client: el.client,
-            type: el.type,
-            web: el.web,
-            link: el.link,
-            description: el.description,
-            description2: el.description2,
-            description3: el.description3,
-            description4: el.description4,
-            description5: el.description5,
-            description6: el.description6,
-            image: el.image,
-            slide01: el.slide01,
-            slide02: el.slide02,
-          };
-  
-          setProjects((projects) => [...projects, project]);
-          console.log(projects)
-        });
-      };
-  
-      getProjects();
-    }, []);
+  const filteredProjects = projectsDB.filter((singleProject)=>{
+    return singleProject.category.includes(filter)
+  })
 
-
-    useEffect(() => {
-      setProjects([]);
-  
-      const filtered = projectsDB.map(p => ({
-        ...p,
-        filtered: p.category.includes(filter)
-      }));
-      setProjects(filtered);
-    }, [filter]);
-
-
-    const handleShowProjects = ()=> {
-        setShow(!show);
-    };
-
-
-  
-    return (
-      <>
-      
+  return (
+    <>
       <div className="portfolio_labels">
-              <button active={filter === "all"} onClick={() => setFilter("all")}>
-                TODOS
-              </button>
-              <button active={filter === "diseno"} onClick={() => setFilter("diseno")}>
-                DISEÑO GRÁFICO
-              </button>
-              <button active={filter === "web"} onClick={() => setFilter("web")}>
-                DISEÑO WEB
-              </button>      
+        <button
+        style={{
+          pointerEvents:`${filter === 'all' ? 'none' :'auto'}`
+        }}
+          active={(filter === 'all').toString()}
+          onClick={() => setFilter('all')}
+        >
+          TODOS
+        </button>
+
+        <button
+        style={{
+          pointerEvents:`${filter === 'diseno' ? 'none' :'auto'}`
+        }}
+          active={(filter === 'diseno').toString()}
+          onClick={() => setFilter('diseno')}
+        >
+          DISEÑO GRÁFICO
+        </button>
+
+        <button
+        style={{
+          pointerEvents:`${filter === 'web' ? 'none' :'auto'}`
+        }}
+          active={(filter === 'web').toString()}
+          onClick={() => setFilter('web')}
+        >
+          DISEÑO WEB
+        </button>
       </div>
 
       <div className="PortfolioGrid">
-        { !show ? (
-          projects.slice(0,10).map((el) => (
-            el.filtered === true ? <Modals key={el.id} client={el.client} type={el.type} web={el.web} link={el.link} description={el.description} description2={el.description2} description3={el.description3} description4={el.description4} description5={el.description5} description6={el.description6} image={el.image} slide01={el.slide01} slide02={el.slide02} /> : ""
-          ))
-        ) : (
-          projects.map((el) => (
-            el.filtered === true ? <Modals key={el.id} client={el.client} type={el.type} web={el.web} link={el.link} description={el.description} description2={el.description2} description3={el.description3} description4={el.description4} description5={el.description5} description6={el.description6} image={el.image} slide01={el.slide01} slide02={el.slide02} /> : ""
-          ))
-        )}
-        </div>
+        {filteredProjects.slice(0, projectFakePagination).map(el => (
+          <Modals
+            key={el.id}
+            client={el.client}
+            type={el.type}
+            web={el.web}
+            link={el.link}
+            description={el.description}
+            description2={el.description2}
+            description3={el.description3}
+            description4={el.description4}
+            description5={el.description5}
+            description6={el.description6}
+            image={el.image}
+            slide01={el.slide01}
+            slide02={el.slide02}
+          />
+        ))}
+      </div>
 
-        <button className="contacto_boton" onClick={handleShowProjects}>
-            {show ? "VER MENOS" : "VER MÁS"}
-        </button>
-      </>
-    );
-  }
-
-
-
-
+      <button className="contacto_boton" onClick={handlePagination}>
+        {projectFakePagination >= filteredProjects.length
+          ? 'VER MENOS'
+          : 'VER MÁS'}
+      </button>
+    </>
+  );
+}
