62ba44faa8aa9392310569d727d737901aea3282
D3press3d <anthonyrosman20@gmail.com>
2023-02-26 13:34:19 -0400

    delete unused data

--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -1,64 +1,32 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import testimoniosDB from "./TestimoniosDB";
-import OwlCarousel from 'react-owl-carousel';  
-import 'owl.carousel/dist/assets/owl.carousel.css';  
-import 'owl.carousel/dist/assets/owl.theme.default.css';  
-import { TestimonioSlide } from "./TestimonioSlide";
+import React from 'react';
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import { TestimonioSlide } from './TestimonioSlide';
 
+const options = {
+  nav: true,
+  rewind: true,
+  autoplay: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 2,
+    },
+  },
+};
 
 export const Testimonio = () => {
-
-    const [, setTestimonios] = useState([]);
-
-    const options = {
-        nav: true,
-        rewind: true,
-        autoplay: false,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:2
-            }
-        }
-    };
-     
-    
-
-    useEffect(() => {
-
-      const getTestimonios = () => {
-        
-        testimoniosDB.forEach((el) => {
-  
-            let testimonio = {
-                id: el.id,
-                image: el.image,
-                client: el.client,
-                company: el.company,
-                text: el.text,
-            };
-    
-            setTestimonios((testimonios) => [...testimonios, testimonio]);
-          });
-      }
-    
-      getTestimonios();
-    }, []);
-    
-
-    return (
-
-        <div className="testimonios-slide">
-            <OwlCarousel className="owl-theme" {...options}>
-                <TestimonioSlide />
-            </OwlCarousel>         
-        </div>
-
-    )
-}
+  return (
+    <div className="testimonios-slide">
+      <OwlCarousel className="owl-theme" {...options}>
+        <TestimonioSlide />
+      </OwlCarousel>
+    </div>
+  );
+};
